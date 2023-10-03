fa8e9f01d9dbf7f1b12df21b125f4f51446def3f
Catlover035 <146782683+Catlover035@users.noreply.github.com>
2023-10-02 20:48:05 -0400

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,32 @@
+namespace SpriteKind {
+    export const Follower = SpriteKind.create()
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    Candy = darts.create(assets.image`myImage`, SpriteKind.Projectile, 0, 0)
+    Candy.follow(Bob)
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
+    sprites.destroyAllSpritesOfKind(SpriteKind.Player, effects.spray, 1000)
+    pause(1000)
+    game.setGameOverMessage(false, "That’s not spooky-!")
+    game.gameOver(false)
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Follower, function (sprite, otherSprite) {
+    sprites.destroyAllSpritesOfKind(SpriteKind.Follower, effects.spray, 1000)
+    game.showLongText("PUUMP NOO", DialogLayout.Top)
+})
+sprites.onDestroyed(SpriteKind.Enemy, function (sprite) {
+    pause(100)
+    game.setGameOverMessage(true, "It’s Spooky Month!")
+    game.gameOver(true)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprites.destroy(Bob, effects.spray, 1000)
+})
+let Candy: Dart = null
+let Bob: Sprite = null
 tiles.setCurrentTilemap(tilemap`level`)
-let Pump = sprites.create(assets.image`Pump Sprite`, SpriteKind.Player)
+let Pump = sprites.create(assets.image`Pump Sprite`, SpriteKind.Follower)
 animation.runImageAnimation(
 Pump,
 assets.animation`8 PuGmpmyAnim o`,
@@ -14,10 +41,10 @@ assets.animation`C Skid T`,
 false
 )
 scene.cameraFollowSprite(Skid)
-controller.moveSprite(Skid)
-Pump.follow(Skid, 90)
+controller.moveSprite(Skid, 100, 100)
+Pump.follow(Skid, 95)
 pause(1500)
-let Bob = sprites.create(assets.image`Bob Sprite`, SpriteKind.Enemy)
+Bob = sprites.create(assets.image`Bob Sprite`, SpriteKind.Enemy)
 animation.runImageAnimation(
 Bob,
 assets.animation`Bob l`,
